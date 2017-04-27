64d7c9f06d2db18c5e39b4814e0c175a20470357
Gaurav Koley <arkokoley@live.in>
2017-04-27 13:25:51 +0530

    details

--- render_process/src/search/menu/About.js
+++ render_process/src/search/menu/About.js
@@ -6,9 +6,9 @@ const About = React.createClass({
     render: function() {
         return <div className="about">
             <p>
-                Hosted with &#10084; on <a href="https://github.com/snskrt/desktop-dict" target="_blank">GitHub</a>.
+                Hosted with &#10084; on <a href="https://github.com/arkokoley/sandhi-splitter" target="_blank">GitHub</a>.
                 <br/>
-                Post there any issues or email Daniel at <a href="mailto:mail@snskrt.xyz">mail@snskrt.xyz</a>
+                Post there any issues or email Gaurav at <a href="mailto:gaurav.koley@iiitb.org">gaurav.koley@iiitb.org</a>
             </p>
         </div>
     }
